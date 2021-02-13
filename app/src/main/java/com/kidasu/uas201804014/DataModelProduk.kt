62e216015345f331c61ee3255b524c4cd6b6e018
KidasuXStream <irontank48@gmail.com>
2021-02-13 13:14:46 +0700

    Initial commit

---
+++ app/src/main/java/com/kidasu/uas201804014/DataModelProduk.kt
@@ -0,0 +1,3 @@
+package com.kidasu.uas201804014
+
+data class DataModelProduk (var id: String ="", var name: String ="", var price: String ="")
---
+++ app/src/main/java/com/kidasu/uas201804014/DataModelUser.kt
@@ -0,0 +1,4 @@
+package com.kidasu.uas201804014
+
+data class DataModelUser (var email: String ="", var pass: String ="", var
+fullname: String ="", var alamat: String= "", var jeniskelamin: String = "")
